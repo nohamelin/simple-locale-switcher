36fa9959088b891760969f1ea065b2f15cc419f6
Carlos Mella <nohamelin@outlook.com>
2013-10-03 23:14:47 -0300

    Switch from the Add-ons Manager (first iteration)

--- chrome/content/extensions.js
+++ chrome/content/extensions.js
@@ -6,8 +6,86 @@
 
 var simplels = {
 
-    openGetMoreLanguagesPage: function() {
+    langsvc: null,
+
+    strings: null,
+    localeStrings: { languageNames: null, regionNames: null, formats: null },
+
+
+    handleEvent: function(event) {
+        switch (event.type) {
+            case "load" :
+                this.onLoad();
+                break;
+
+            case "unload" :
+                this.onUnload();
+                break;
+        }
+    },
+
+
+    onLoad: function() {
         Cu.import("resource://simplels/general.jsm", this);
+        Cu.import("resource://simplels/language.jsm", this);
+
+        this.langsvc = Cc["@nohamelin/sls/language-service;1"]
+                       .getService().wrappedJSObject;
+
+        this.strings = document.getElementById("simplels-strings");
+
+        this.localeStrings.languageNames = document.getElementById(
+                                                "simplels-language-names");
+        this.localeStrings.regionNames = document.getElementById(
+                                                "simplels-region-names");
+        this.localeStrings.formats = document.getElementById(
+                                                "simplels-name-formats");
+
+        Services.obs.addObserver(this, "sls:matching-changed", false);
+        Services.obs.addObserver(this, "sls:selected-changed", false);
+
+        this.updateSelectedItem();
+    },
+
+
+    onUnload: function() {
+        Services.obs.removeObserver(this, "sls:matching-changed");
+        Services.obs.removeObserver(this, "sls:selected-changed");
+    },
+
+
+    updateSelectedItem: function() {
+        let selectedItem = document.getElementById("simplels-selected");
+
+        if (this.langsvc.matchingOS) {
+            selectedItem.value = this.strings.getString("selected.match");
+            selectedItem.tooltipText = "";
+        }
+        else {
+            selectedItem.value = this.getLocaleName(this.langsvc.userLocale);
+            selectedItem.tooltipText = this.langsvc.userLocale;
+        }
+    },
+
+
+    observe: function(subject, topic, data) {
+        switch (topic) {
+            case "sls:matching-changed" :
+            case "sls:selected-changed" :
+                this.updateSelectedItem();
+                break;
+        }
+    },
+
+
+    openLanguagesDialog: function() {
+        let langsURL = "chrome://simplels/content/languages-outer.xul";
+
+        this.utils.openPreferencesWindow(window, langsURL);
+    },
+
+
+    openGetMoreLanguagesPage: function() {
         Cu.import("resource://simplels/urlFormatter.jsm", this);
 
         let prefs = Services.prefs.getBranch("extensions.simplels.");
@@ -24,5 +102,14 @@ var simplels = {
                 this.tbUtils.openContentTab(getURL);
                 break;
         }
+    },
+
+
+    getLocaleName: function(locale) {
+        return this.langUtils.buildLocaleName(locale, this.localeStrings);
     }
 };
+
+
+document.addEventListener("load", simplels, true);
+window.addEventListener("unload", simplels);
