68dc344e9048c5d7adedadff9d8d37d6d949c996
Carlos Mella <nohamelin@outlook.com>
2014-03-01 19:52:46 -0300

    Add a preferences window for the extension

---
+++ chrome/content/preferences.js
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+
+var simplels = {
+
+    getMoreLanguagesPref: null,
+
+    customBcstr: null,
+    customListItem: null,
+
+
+    onLoad: function() {
+        this.getMoreLanguagesPref = document.getElementById("getMoreLanguages");
+
+        this.customBcstr = document.getElementById("customDestination");
+        this.customListItem = document.getElementById(
+                                            "custom-destination-list-item");
+
+        this.onChangeGetMorePreference();
+    },
+
+
+    onChangeGetMorePreference: function() {
+        if (this.getMoreLanguagesPref.value != this.customListItem.value)
+            this.customBcstr.setAttribute("disabled", "true");
+        else
+            // removeAttribute doesn't raise an exception if the attribute
+            // isn't found.
+            this.customBcstr.removeAttribute("disabled");
+    }
+};
